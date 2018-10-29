691da7f765f48a06a7c7bfb9afe5b8ceb42f8144
Weisheng Wu <weishengw@outlook.com>
2018-10-29 01:52:59 -0700

    created sender 257 structure

---
+++ Sender_257.cpp
@@ -0,0 +1,33 @@
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstdlib>
+#include <bits/stdc++.h>
+#include "get_info.h"
+using namespace std;
+
+void get_info(int, struct msgbuf *, int, long);
+
+int main() {
+
+
+
+	int qid = msgget(ftok(".",'u'), 0);
+
+	// declare my message buffer
+	struct buf {
+		long mtype; // required
+		char greeting[50]; // mesg content
+	};
+	buf msg;
+	int size = sizeof(msg)-sizeof(long);
+
+
+
+}
